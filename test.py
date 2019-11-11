379f5e9264fa26901d3766a231900a090fe50dc1
bartOssh <lenart.consulting@gmail.com>
2019-11-11 21:33:35 +0100

    update and clean python test script

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import requests
 
+num_of_iter = 2
+
 data = open('./assets/test.jpg', 'rb').read()
-for i in range(0, 10):
+for i in range(0, num_of_iter):
     res = requests.get(
         url='http://0.0.0.0:8000/recognition/object/boxes_names'
     )
@@ -10,10 +12,10 @@ for i in range(0, 10):
     res = requests.post(url='http://0.0.0.0:8000/recognition/object/boxes',
                         data=data,
                         headers={'Content-Type': 'application/octet-stream'})
-    print("\n RESPONSE POST image receive image for test number {}: \n {}"
-        .format(i, res.__dict__))
+    print("\n RESPONSE POST to boxes, test num {} \n Sending buffer length: {},\n Received {}"
+        .format(i, len(data), res.__dict__))
     res = requests.post(url='http://0.0.0.0:8000/recognition/object/image',
                         data=data,
                         headers={'Content-Type': 'application/octet-stream'})
-    print("\n RESPONSE POST image receive boxes for test number {}: \n {}"
-        .format(i, res.__dict__))
+    print("\n RESPONSE POST to image, test num {} \n Sending buffer length: {},\n Received {}"
+        .format(i, len(data), res))
