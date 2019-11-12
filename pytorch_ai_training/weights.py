b84f00a4058ce8777f338e2589f9e4a8bd723952
bartOssh <lenart.consulting@gmail.com>
2019-11-12 23:10:40 +0100

    set posibility to download not trained models

--- pytorch_ai_training/weights.py
+++ pytorch_ai_training/weights.py
@@ -4,23 +4,30 @@ import scipy.stats as stats
 
 
 vision_models_obj = {
-    'resnet18': models.resnet18(pretrained=True),
-    'alexnet': models.alexnet(pretrained=True),
-    'vgg16': models.vgg16(pretrained=True),
-    'squeezenet': models.squeezenet1_0(pretrained=True),
-    'densenet': models.densenet161(pretrained=True),
-    'inception': models.inception_v3(pretrained=True),
-    'googlenet': models.googlenet(pretrained=True),
-    'shufflenet': models.shufflenet_v2_x1_0(pretrained=True),
-    'mobilenet': models.mobilenet_v2(pretrained=True),
-    'resnext50_32x4d': models.resnext50_32x4d(pretrained=True),
-    'wide_resnet50_2': models.wide_resnet50_2(pretrained=True),
-    'mnasnet': models.mnasnet1_0(pretrained=True)
+    'resnet18': models.resnet18,
+    'alexnet': models.alexnet,
+    'vgg16': models.vgg16,
+    'squeezenet': models.squeezenet1_0,
+    'densenet': models.densenet161,
+    'inception': models.inception_v3,
+    'googlenet': models.googlenet,
+    'shufflenet': models.shufflenet_v2_x1_0,
+    'mobilenet': models.mobilenet_v2,
+    'resnext50_32x4d': models.resnext50_32x4d,
+    'wide_resnet50_2': models.wide_resnet50_2,
+    'mnasnet': models.mnasnet1_0
 }
 
 
-def get_pretrained_nn(nn_type):
-    nn_model = vision_models_obj.get(nn_type, 'resnet18')
+def get_pretrained_vision_nn(nn_type):
+    nn_model = vision_models_obj.get(nn_type, 'resnet18')(pretrained=True,
+        progress=True)
+    print(nn_model)
+
+
+def get_vision_nn(nn_type):
+    nn_model = vision_models_obj.get(nn_type, 'resnet18')(pretrained=False,
+        progress=True)
     print(nn_model)
 
 
@@ -31,8 +38,14 @@ if __name__ == '__main__':
         from pytorch ecosystem or trained locally')
     parser.add_argument('-p', '--pretrained', type=str, nargs=1,
                         metavar='neron_network_type', default=None,
-                        help='Specifies what pre-trained weights \
+                        help='Specifies what pre-trained nn weights \
                         to provide')
+    parser.add_argument('-m', '--model', type=str, nargs=1,
+                        metavar='neron_network_type', default=None,
+                        help='Specifies what model of nn we wont to train \
+                        for weights')
     args = parser.parse_args()
     if args.pretrained is not None:
-        get_pretrained_nn(args.pretrained[0])
+        get_pretrained_vision_nn(args.pretrained[0])
+    if args.model is not None:
+        get_vision_nn(args.model[0])
